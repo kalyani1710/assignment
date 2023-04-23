615c9a9ca12d26006f56c727509181ae8debcc51
kalyani1710 <131145510+kalyani1710@users.noreply.github.com>
2023-04-23 11:51:43 +0530

    Add files via upload

---
+++ perfect.py
@@ -0,0 +1,11 @@
+row=int(input("Enter a new line:"))
+def line(a):
+space= " * n
+print("the first row")
+      for(row in range(1,n+1))
+print("space,end=")
+      for (in range(row))
+print one starless print("+",end='')
+print()
+space=space[i-1]
+line(a)
---
+++ vowel.py
@@ -0,0 +1,5 @@
+row= int(input("Enter number of row:"))
+for i in range ("rows"):
+    for j in range (i+1):
+        print("*",end=" ")
+print("\n")        
